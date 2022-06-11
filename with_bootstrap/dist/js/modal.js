94921c76c7dc241d0c20e5d2fad55bd64d4bdbf3
mosquitoesix <znaa33@gmail.com>
2022-06-12 00:08:58 +0200

    map examples added

--- with_bootstrap/dist/js/modal.js
+++ with_bootstrap/dist/js/modal.js
@@ -1,14 +1,18 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img");
+var captionText = document.getElementById("caption");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+var imgList = document.getElementsByClassName("modal-img");
+
+for(var i=0;i < imgList.length; i++) {
+  var image = imgList[i];
+  image.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
 }
 
 // Get the <span> element that closes the modal
