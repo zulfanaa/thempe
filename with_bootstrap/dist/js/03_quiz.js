10713485ec0486111a1bd45f51be3ef1de6a997a
mosquitoesix <znaa33@gmail.com>
2022-06-14 18:02:31 +0200

    problem fixed

--- with_bootstrap/dist/js/03_quiz.js
+++ with_bootstrap/dist/js/03_quiz.js
@@ -1,6 +1,6 @@
 (function() {
     var questions = [{
-      question: "what kind of quantitative data can be visualized in a Choropleth Map??",
+      question: "what kind of quantitative data can be visualized in a Choropleth Map?",
       choices: [' abrupt and discrete',
       ' smooth and continous',
       ' smooth and discrete',
